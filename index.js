2baee47fdee5e82aca3bf7d34c57307fa66a6d8e
Ana <acsf6@aluno.ifnmg.edu.br>
2023-08-04 13:43:47 -0300

    Adicionei novas coisas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { selectUsuarios } from "./bd.js";
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
@@ -11,4 +13,16 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+dotenv.config();
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
