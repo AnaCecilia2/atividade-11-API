4d46c414a2770d69edb07042a10156e6464c4a1e
Ana <acsf6@aluno.ifnmg.edu.br>
2023-08-25 14:07:50 -0300

    autenticação feita

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { selectUsuarios, selectUsuario, insertUsuario,  deleteUsuario, updateUsu
 import dotenv from "dotenv";
 import express from "express";  
 import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const port = 3000;
 
 app.use(express.json());
 app.use(roteadorUsuario);
+app.use(roteadorLogin);
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {       
